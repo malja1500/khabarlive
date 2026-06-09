44890b2b926d7df5fb6cf019918ab3e60a350cde
malja1500 <mianroudianf@gmail.com>
2026-06-09 13:50:27 +0330

    some new update

--- next.config.js
+++ next.config.js
@@ -6,13 +6,6 @@ const nextConfig = {
       { protocol: 'https', hostname: 'i.pravatar.cc' },
     ],
   },
-  // Required for better-sqlite3 to work in Next.js
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), 'better-sqlite3'];
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
